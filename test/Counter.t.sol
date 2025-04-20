2ec1c36237ccb7d1b2bcf317e510d5a18ef54a7e
vvv <406666169@qq.com>
2025-04-20 16:39:52 +0800

    fix: remove incremnt in counter.t.sol

--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -12,11 +12,6 @@ contract CounterTest is Test {
         counter.setNumber(0);
     }
 
-    function test_Increment() public {
-        counter.increment();
-        assertEq(counter.number(), 1);
-    }
-
     function testFuzz_SetNumber(uint256 x) public {
         counter.setNumber(x);
         assertEq(counter.number(), x);
